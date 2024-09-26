97208310a638cedfd11fa0d52ae70b623d4311e4
Ashleen <ashleen.daly@glasgow.ac.uk>
2024-09-26 19:28:04 +0100

    use vector to store users

--- auth/src/lib.rs
+++ auth/src/lib.rs
@@ -4,26 +4,53 @@ pub enum LoginAction {
     Denied,
 }
 
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Clone)]
 pub enum LoginRole {
     Admin,
     User,
 }
 
-pub fn login(username: &str, password: &str) -> Option<LoginAction> {
-    let username = username.to_lowercase();
+pub struct User {
+    pub username: String,
+    pub password: String,
+    pub role: LoginRole,
+}
 
-    if username != "admin" && username != "ash" {
-        return None
+impl User {
+    pub fn new(username: &str, password: &str, role: LoginRole) -> User {
+        User {
+            username: username.to_lowercase(),
+            password: password.to_string(),
+            role
+        }
     }
-    
-    if username == "admin" && password == "password" {
-        Some(LoginAction::Granted(LoginRole::Admin))
-    } else if username == "ash" && password == "password" {
-        Some(LoginAction::Granted(LoginRole::User))
-    } else {
-        Some(LoginAction::Denied)
+}
+
+pub fn get_users() -> Vec<User> {
+    vec![
+        User::new("admin", "password", LoginRole::Admin),
+        User::new("ash", "password", LoginRole::User),
+    ]
+}
+
+pub fn get_admin_usernames() -> Vec<String> {
+    get_users()
+        .into_iter()
+        .filter(|u| u.role == LoginRole::Admin)
+        .map(|u| u.username)
+        .collect()
+}
+
+pub fn login(username: &str, password: &str) -> Option<LoginAction> {
+    let users = get_users();
+    if let Some(user) = users.iter().find(|user| user.username == username.to_lowercase()) {
+        if user.password == password {
+            return Some(LoginAction::Granted(user.role.clone()))
+        } else {
+            return Some(LoginAction::Denied)
+        }
     }
+    None
 }
 
 pub fn read_line() -> String {
@@ -34,13 +61,10 @@ pub fn read_line() -> String {
 
 #[cfg(test)]
 mod tests {
-    // use super::*;
+    use super::*;
 
-    // #[test]
-    // fn test_login() {
-    //     assert_eq!(login("admin", "password"), LoginAction::Granted(LoginRole::Admin));
-    //     assert_eq!(login("ADMIN", "password"), LoginAction::Granted(LoginRole::Admin));
-    //     assert_eq!(login("ash", "password"), LoginAction::Granted(LoginRole::User));
-    //     assert_eq!(login("ash", "not-password"), LoginAction::Denied);
-    // }
+    #[test]
+    fn test_get_admin_usernames() {
+        assert_eq!(get_admin_usernames(), vec!["admin"])
+    }
 }
