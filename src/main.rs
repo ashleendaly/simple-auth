c947a609730e76825faa1b2bb73eda9223ec8618
Ashleen <ashleen.daly@glasgow.ac.uk>
2024-09-26 16:26:29 +0100

    Use match to determine what to print to the user based on the enum returned from the login attempt

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use auth::{login, read_line};
+use auth::*;
 
 fn main() {
     let mut tries: i32 = 0;
@@ -7,16 +7,27 @@ fn main() {
         let username = read_line();
         println!("Enter your password:");
         let password = read_line();
-        if login(&username, &password) {
-            println!("Welcome");
-            break
-        } else {
-            println!("Incorrect username or password");
-            tries += 1;
-            if tries >= 3 {
-                println!("Too many failed attempts");
-                break
+
+        match login(&username, &password) {
+            Some(LoginAction::Granted(role)) => {
+                match role {
+                    LoginRole::Admin => println!("Admin"),
+                    LoginRole::User => println!("User"),
+                }
+                break;
             }
+            Some(LoginAction::Denied) => {
+                println!("Incorrect password, access denied")
+            }
+            None => {
+                println!("User does not exist")
+            }
+        }
+
+        tries += 1;
+        if tries >= 3 {
+            println!("Too many failed attempts");
+            break
         }
     }
 }
